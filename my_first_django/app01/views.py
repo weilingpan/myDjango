6669b942464ea9b780d275ee622c6b0662789ae7
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-08 11:51:42 +0800

    add app01/route03

--- my_first_django/app01/views.py
+++ my_first_django/app01/views.py
@@ -16,9 +16,17 @@ def route1(request):
 #     else:
 #         print("No project token provided")
 #         return HttpResponse("Hello world! app01, No project token")
+# path('route1/', views.route1, name='myroute1')
 # http://127.0.0.1:8000/app01/route1/?project_token=123
 # http://127.0.0.1:8000/app01/route1/
 
 @ensure_csrf_cookie
 def route2(request):
     return HttpResponse("route2: ensure_csrf_cookie")
+
+# 這個參數是從 URL 路徑中提取的，而不是從查詢參數中提取的
+def route3(request, project_token):
+    print(f"Project Token: {project_token}")
+    return HttpResponse(f"Route3: Project Token: {project_token}")
+#http://127.0.0.1:8000/app01/route3/123/
+#path('route3/<str:project_token>/', views.route3, name='route3')
