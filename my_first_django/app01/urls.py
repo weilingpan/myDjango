b31d9e51c5eff5f017951da0763a7ac59a831b74
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-08 11:52:13 +0800

    add app01/route03 to urls.py

--- my_first_django/app01/urls.py
+++ my_first_django/app01/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('route1/', views.route1, name='myroute1'),
     path('route2/', views.route2, name='route2'),
+    path('route3/<str:project_token>/', views.route3, name='route3')
 ]
