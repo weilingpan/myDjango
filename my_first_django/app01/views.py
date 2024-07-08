234d0e86e0b595af608dfdc21baf37eb0a81f91d
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-08 10:56:34 +0800

    Update views.py

--- my_first_django/app01/views.py
+++ my_first_django/app01/views.py
@@ -7,6 +7,18 @@ from django.http import HttpResponse
 def route1(request):
     return HttpResponse("Hello world! app01")
 
+# def route1(request):
+#     print(request.GET.keys)
+#     project_token = request.GET.get('project_token', None)
+#     if project_token:
+#         print(f"Project Token: {project_token}")
+#         return HttpResponse(f"Hello world! app01, Project Token: {project_token}")
+#     else:
+#         print("No project token provided")
+#         return HttpResponse("Hello world! app01, No project token")
+# http://127.0.0.1:8000/app01/route1/?project_token=123
+# http://127.0.0.1:8000/app01/route1/
+
 @ensure_csrf_cookie
 def route2(request):
     return HttpResponse("route2: ensure_csrf_cookie")
