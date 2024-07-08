2d7bafae69e2cbb7e0d7447c7e736b20c1f5cde0
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-08 17:09:35 +0800

    Update urls.py

--- my_first_django/app01/urls.py
+++ my_first_django/app01/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('route1/', views.route1, name='myroute1'),
     path('route2/', views.route2, name='route2'),
-    path('route3/<str:project_token>/', views.route3, name='route3')
+    path('route3/<str:project_token>/', views.route3, name='route3'),
+    path('route4/<str:is_ok>/', views.route4, name='route4'),
+    path('route5/', views.route5, name='route5'),
 ]
