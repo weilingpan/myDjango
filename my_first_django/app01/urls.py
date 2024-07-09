082eccf690fcd224ea4dadc31038d5782dc45c79
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-09 15:18:31 +0800

    Update urls.py: for swagger, add new api

--- my_first_django/app01/urls.py
+++ my_first_django/app01/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('route3/<str:project_token>/', views.route3, name='route3'),
     path('route4/<str:is_ok>/', views.route4, name='route4'),
     path('route5/', views.route5, name='route5'),
+
+    path('hello/', views.HelloWorld.as_view(), name='hello_world'),
 ]
