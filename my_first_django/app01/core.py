079b874326d4c302135b28d7d624093642929053
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-16 17:46:11 +0800

    Update core.py

--- my_first_django/app01/core.py
+++ my_first_django/app01/core.py
@@ -1,6 +1,14 @@
 import django_rq
 from rq.job import Job
-from rq.registry import FinishedJobRegistry
+
+# 用於查詢不同狀態的jobs
+from rq.registry import (
+    FinishedJobRegistry, #已完成的任務
+    FailedJobRegistry, #失敗的任務
+    StartedJobRegistry, #正在進行的任務
+    DeferredJobRegistry, #被推遲的任務
+    ScheduledJobRegistry #計畫中的任務
+    )
 
 """
 django_rq 的一些用法
