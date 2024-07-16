217ad555f0bc6aeedb0e04b52585822e5f22027a
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-16 17:38:10 +0800

    Update core.py

--- my_first_django/app01/core.py
+++ my_first_django/app01/core.py
@@ -2,15 +2,14 @@ import django_rq
 from rq.job import Job
 from rq.registry import FinishedJobRegistry
 
-def get_all_jobs():
-    queue = django_rq.get_queue('default')
-    job_ids = queue.job_ids # 取所有任務的id
-    jobs = [Job.fetch(job_id, connection=queue.connection) for job_id in job_ids]
-    return jobs
+"""
+django_rq 的一些用法
+"""
+# scheduler = django_rq.get_scheduler()
 
 def get_finished_jobs():
     queue = django_rq.get_queue('default')
     finished_job_registry = FinishedJobRegistry(queue=queue)
     job_ids = finished_job_registry.get_job_ids()
-    # jobs = [Job.fetch(job_id, connection=queue.connection) for job_id in job_ids]
-    return job_ids
+    jobs = [Job.fetch(job_id, connection=queue.connection) for job_id in job_ids]
+    return job_ids, jobs
