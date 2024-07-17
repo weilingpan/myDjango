8793a4865a651c433dfad8c113ac242932537740
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-17 13:53:39 +0800

    Update tests.py - for app01/route01/

--- my_first_django/app01/tests.py
+++ my_first_django/app01/tests.py
@@ -1,3 +1,18 @@
+import pytest
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 # Create your tests here.
+class TestExample(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_route1(self):
+        response = self.client.get('/app01/route1/')
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
