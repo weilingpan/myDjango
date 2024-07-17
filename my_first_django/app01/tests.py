af618bbf6f97ad99c022c91d4dc8b7c51d9679a1
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-17 14:19:46 +0800

    Update tests.py

--- my_first_django/app01/tests.py
+++ my_first_django/app01/tests.py
@@ -21,3 +21,37 @@ class TestExample(TestCase):
         response = self.client.get('/app01/route1/')
         print(response.json())
         self.assertEqual(response.status_code, 200)
+
+class Test01(TestCase):
+    # 每一個測試方法執行前執行
+    def setUp(self):
+        print("======= start =======")
+
+    def test01(self):
+        print("run test01")
+
+    def test02(self):
+        print("run test02")
+
+    # 每一個測試方法執行後執行
+    def tearDown(self):
+        print("======= end =======")
+
+
+class Test(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("start===============!")
+
+    def test01(self):
+        print("run test01")
+
+    def test02(self):
+        print("run test02")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("end!===================")
+
+# python manage.py test
+# python manage.py test -v=2 # 顯示詳細資訊
