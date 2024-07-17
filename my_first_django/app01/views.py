b0da31671e4f5730706e463b964b36cbf4451682
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-17 11:27:01 +0800

    remove app01/views.py FirstViewSet

--- my_first_django/app01/views.py
+++ my_first_django/app01/views.py
@@ -81,31 +81,3 @@ from rest_framework import status
 class HelloWorld(APIView):
     def get(self, request):
         return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
-
-
-# 1. 定義 viewsets, 需包含（如 list、retrieve、create、update、destroy 等）。
-# 2. 使用 routers 在 urls.py 註冊
-# 3. 在 viewsets 自定義 CRUD API
-from rest_framework import status, viewsets
-class FirstViewSet(viewsets.ViewSet):
-    iam_organization_field = None
-    serializer_class = None
-
-    # get
-    def list(self, request):
-        """
-        新增API的說明可以放在這裡
-        """
-        return HttpResponse('get list')
-    
-    # post
-    def create(self, request):
-        return HttpResponse('create')
-    
-    # get
-    def retrieve(self, request, pk=None):
-        return HttpResponse('retrieve by pk')
-    
-    # delete
-    def destroy(self, request, pk=None):
-        return HttpResponse('delete by pk')
