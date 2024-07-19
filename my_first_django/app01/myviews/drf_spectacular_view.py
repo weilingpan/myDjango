1bbc1678cf7f3478c1fe598403eb8551a68e0795
Regina <42767268+weilingpan@users.noreply.github.com>
2024-07-19 15:28:57 +0800

    Update drf_spectacular_view.py: add ensure_csrf_cookie

--- my_first_django/app01/myviews/drf_spectacular_view.py
+++ my_first_django/app01/myviews/drf_spectacular_view.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError, JsonResponse
-
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
 
 @extend_schema(tags=['regina-api'])
 @extend_schema_view(
@@ -19,6 +20,7 @@ class ReginaViewSet(viewsets.ViewSet):
     def get_serializer(self, *args, **kwargs):
         pass
 
+    @method_decorator(ensure_csrf_cookie)
     def list(self, request):
         print("abb")
         return JsonResponse({"message": "Hello world!"})
